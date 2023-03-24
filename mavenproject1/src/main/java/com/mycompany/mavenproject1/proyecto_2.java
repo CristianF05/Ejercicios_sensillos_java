d36fa4c6cbe470e2b04e7f402afa358efffb777e
CristianF05 <1482670@senati.pe>
2023-03-24 11:08:36 -0500

    practica 2

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/proyecto_2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+ import java.util.Scanner;
+/**
+ *
+ * @author Alumno
+ */
+public class proyecto_2 {
+     public static void main(String[] args) {
+         Scanner v = new Scanner(System.in);
+         System.out.println("numero par: ");
+         int n1 = v.nextInt();
+         System.out.println("numero par: ");
+         int n2 = v.nextInt();
+         if(n1 != n2)
+         {
+             System.out.println(n1+"!="+n2+"los numerso diferentes");
+         }
+         
+     
+}
+}
