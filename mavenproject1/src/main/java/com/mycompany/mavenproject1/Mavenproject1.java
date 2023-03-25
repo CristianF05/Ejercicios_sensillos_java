b34bab0da5d67cc696aaa88e82ff99d98b5853e7
CristianF05 <1482670@senati.pe>
2023-03-25 08:20:56 -0500

    texto

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Morning, Morning,Morning");
+        System.out.println("        Hello, Have a good day");
+        System.out.println("                oh! Thank you");
+        System.out.println("                             ");
+        System.out.println("                You are great");
+        System.out.println("        This is my pleasure");
+        System.out.println("No,... You are welcome");
+    }
+}
