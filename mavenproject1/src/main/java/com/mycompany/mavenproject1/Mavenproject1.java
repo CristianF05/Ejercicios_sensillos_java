5229aaa5baed6719fa674d71a6659db848a531a9
CristianF05 <1482670@senati.pe>
2023-03-25 09:48:53 -0500

    ingresando valor inicial y valor final

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -12,12 +12,24 @@ public class Mavenproject1 {
 
     public static void main(String[] args) {
         Scanner valor = new Scanner(System.in);
-        System.out.println("ingrese un numero a evaluar: ");
+        int n;
+        System.out.println("ingrese numero inicial: ");
+        int i = valor.nextInt();
+        System.out.println("ingrese numero final: ");
         int m = valor.nextInt();
         System.out.println("numeros inpares: ");
-        for (int i = 1;i<=m;i+=2){
-            System.out.println("inpar: "+i);
+        if(i %2 ==0){
+                 n = i + 1;
             
+        for (;n<=m;n+=2){
+            
+            System.out.println("inpar: "+n); 
+        }
+        }
+        else {
+        for (;i<=m;i+=2){
+            System.out.println("inpar: "+i);
+        }
         }
     }
 }
